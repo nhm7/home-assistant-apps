29ec9786f7d378a2f380dff17094a7aff3fcdc90
nhm7 <220584148+nhm7@users.noreply.github.com>
2026-09-21 00:02:00 +0200

    Fix Guacamole app startup entrypoint

--- apps/rdp-gateway/run.sh
+++ apps/rdp-gateway/run.sh
@@ -4,5 +4,5 @@ python3 /gateway.py --configure
 export GUACAMOLE_HOME=/etc/guacamole
 export LD_LIBRARY_PATH=/opt/guacamole/lib
 /opt/guacamole/sbin/guacd -b 127.0.0.1 -p 4822 &
-/opt/guacamole/bin/start.sh &
+/opt/guacamole/bin/entrypoint.sh &
 exec python3 /gateway.py
